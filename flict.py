7f4a0443a1f1b814cfda1e372ce747eeb37b4aba
Patrick Goldinger <patrick.goldinger@pm.me>
2021-02-14 14:23:47 +0100

    Clean up flict.py code

--- flict.py
+++ flict.py
@@ -1,5 +1,5 @@
-import json
 import io
+from typing import Dict, Optional
 import unicodedata
 
 __FLICT_CMD_BEGIN_HEADER =      0x1
@@ -10,6 +10,25 @@ __FLICT_BEGIN_SECTION_128 =     0x00
 __FLICT_BEGIN_SECTION_16384 =   0x80
 __FLICT_BEGIN_HEADER =          0xC0
 __FLICT_END =                   0xF0
+
+class FlictNode:
+    def __init__(self, freq: Optional[int] = None):
+        self.children: Dict[str, FlictNode] = {}
+        self.freq: Optional[int] = freq
+
+    # def optimize(self) -> list:
+    #     ret_list = []
+    #     if self.freq != None and len(self.children) == 0:
+    #         # Is opaque end point
+    #         ret_list.insert(0, self)
+    #     elif self.freq == None and len(self.children) == 1:
+    #         self.children[self.children.keys[0]]
+
+class FlictRootNode(FlictNode):
+    def __init__(self):
+        self.children: Dict[str, FlictNode] = {}
+        self.header: Optional[str] = None
+        self.freq: Optional[int] = None
 
 def __encode_cmd_block(byte_arr: bytearray, cmd: int, v: int):
     if cmd == __FLICT_CMD_END:
@@ -43,88 +62,64 @@ def __encode_cmd_block(byte_arr: bytearray, cmd: int, v: int):
     else:
         raise ValueError("Given cmd is invalid!")
 
-def __encode_inner(byte_arr: bytearray, word: str, section: dict):
+def __encode_inner(byte_arr: bytearray, word: str, node: FlictNode):
     word_bytes = bytearray(word, "utf-8")
     __encode_cmd_block(byte_arr, __FLICT_CMD_BEGIN_SECTION, len(word_bytes))
-    if "$f" in section:
-        byte_arr.append(int(section["$f"]) & 0xFF)
+    if node.freq != None:
+        byte_arr.append(node.freq & 0xFF)
     else:
         byte_arr.append(0x00)
     byte_arr.extend(word_bytes)
-    for key in section:
-        if key == "$f":
-            pass
-        else:
-            __encode_inner(byte_arr, key, section[key])
+    for key in node.children:
+        __encode_inner(byte_arr, key, node.children[key])
     last_byte = byte_arr[-1]
     if last_byte & 0xF0 == 0xF0:
-        del byte_arr[-1]
-        __encode_cmd_block(byte_arr, __FLICT_CMD_END, (last_byte & 0x0F) + 1)
+        if last_byte & 0x0F == 15:
+            __encode_cmd_block(byte_arr, __FLICT_CMD_END, 1)
+        else:
+            del byte_arr[-1]
+            __encode_cmd_block(byte_arr, __FLICT_CMD_END, (last_byte & 0x0F) + 1)
     else:
         __encode_cmd_block(byte_arr, __FLICT_CMD_END, 1)
 
-def __encode(obj: dict):
+def __encode(node: FlictRootNode):
     byte_arr = bytearray()
-    for key in obj:
-        if key == "$h":
-            header_str = bytearray(obj[key], "utf-8")
-            __encode_cmd_block(byte_arr, __FLICT_CMD_BEGIN_HEADER, len(header_str))
-            byte_arr.extend(header_str)
-            __encode_cmd_block(byte_arr, __FLICT_CMD_END, 1)
-        else:
-            __encode_inner(byte_arr, key, obj[key])
+    if node.header != None:
+        header_str = bytearray(node.header, "utf-8")
+        __encode_cmd_block(byte_arr, __FLICT_CMD_BEGIN_HEADER, len(header_str))
+        byte_arr.extend(header_str)
+        __encode_cmd_block(byte_arr, __FLICT_CMD_END, 1)
+    for key in node.children:
+        __encode_inner(byte_arr, key, node.children[key])
     return byte_arr
 
 def clb_to_flict(src_path: str, dst_path: str):
     try:
-        flict_prep_obj = {}
-        worddict = {}
+        flict_tree = FlictRootNode()
         with io.open(src_path, "r", encoding="utf-8") as f_src:
             is_first = True
             for line in f_src.readlines():
                 if is_first:
                     # Treat as header
-                    flict_prep_obj.update({"$h": line.rstrip()})
+                    flict_tree.header = line.rstrip()
                     is_first = False
                 else:
                     line_list = line.rstrip()[6:].split(",f=")
                     word = unicodedata.normalize("NFC", line_list[0])
                     freq = int(line_list[1])
-                    worddict.update({word:freq})
-        wordlist = sorted(worddict)
-        wordlist_index = 0
-        for word in wordlist:
-            freq = worddict[word]
-            prep_obj = flict_prep_obj
-            pos = 0
-            for c in word:
-                if pos + 1 >= len(word):
-                    if c not in prep_obj:
-                        prep_obj.update({c:{}})
-                    prep_obj[c].update({"$f": freq})
-                else:
-                    if c in prep_obj:
-                        pass
-                    else:
-                        if wordlist_index + 1 >= len(wordlist):
-                            if c not in prep_obj:
-                                prep_obj.update({c:{}})
-                        elif wordlist[wordlist_index + 1].startswith(word[0:pos + 1]):
-                            if c not in prep_obj:
-                                prep_obj.update({c:{}})
+                    flict_tree_node = flict_tree
+                    pos = 0
+                    for c in word:
+                        if not c in flict_tree_node.children:
+                            flict_tree_node.children[c] = FlictNode(None)
+                        if pos + 1 >= len(word):
+                            flict_tree_node.children[c].freq = freq
                         else:
-                            wr = word[pos:]
-                            if wr not in prep_obj:
-                                prep_obj.update({wr:{}})
-                            prep_obj[wr].update({"$f": freq})
-                            break
-                    prep_obj = prep_obj[c]
-                    pos += 1
-            wordlist_index += 1
-        # with io.open("test.flict.json", "w") as f:
-        #     f.write(json.dumps(flict_prep_obj, separators=(',', ':')))
+                            flict_tree_node = flict_tree_node.children[c]
+                        pos += 1
+
         with io.open(dst_path, "wb") as f_dst:
-            f_dst.write(__encode(flict_prep_obj))
+            f_dst.write(__encode(flict_tree))
         return True
     except ValueError as e:
         print(e)
